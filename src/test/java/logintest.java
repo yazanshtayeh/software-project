ac40ebb9942eb9ed07e2068cb2416178a4781e9f
Mujahed Mohammed <78280184+muja-b@users.noreply.github.com>
2021-03-10 19:56:22 +0200

    updates#7

--- src/test/java/logintest.java
+++ src/test/java/logintest.java
@@ -9,10 +9,17 @@ import static org.junit.Assert.assertTrue;
 
 public class logintest {
      boolean flag = false;
+     int pass;
+     int id;
 
+     @Given("the correct password is {int} and correct id is {int}")
+    public void theCorrectPasswordIsAndCorrectIdIs(int arg0, int arg1) {
+        pass = arg0;
+        id = arg1;
+    }
     @When("user fills id with {int} and password with {int}")
     public void userFillsIdWithAndPasswordWith(int int1, int int2) {
-        if (int1 == 222 && int2 == 222) flag = true;
+        if (int1 == pass && int2 == id) flag = true;
     }
 
     @Then("give admin authorities and go to admin page")
@@ -23,11 +30,14 @@ public class logintest {
    @Then("don't give admin authorities")
     public void don_t_give_admin_authorities() {
         flag=false;
+        assertTrue(true);
     }
 
     @Then("show a wrong info massage")
     public void show_a_wrong_info_massage() {
-    System.out.println("you're not an admin");
+    System.out.println("wrong password");
     }
+
+
 
 }
